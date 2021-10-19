a6a6c9bcac6af20e9811b15f87c576df1546ab3b
Christopher-Devitt <chris.devitt@ucdconnect.ie>
2021-10-19 10:20:41 +0100

    Create question2.c

---
+++ assignment1/question2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<math.h>
+
+float Series(float coefficient, float variable, int power);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function that generates the series of a*r^n
+float Series(float coefficient, float variable, int power)
+{
+	int i = 0;
+	float sum = 0;
+	while(i <= power)
+	{
+		sum += coefficient*pow(variable, i);
+		i += 1;
+	}
+	return(sum);
+}
+
