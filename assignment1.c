cca891dd2d30c18f359a69263be0a3178be8fd6d
Aoife Brennan <aoifebrennan@Aoifes-Mac-mini.local>
2021-10-27 17:56:31 +0100

    adding assignment1 for part (b), (c), (d)

--- assignment1.c
+++ assignment1.c
@@ -1,19 +1,37 @@
-#include <stdio.h>
 #include <math.h>
 
-int main(void){
+void fun(int *n);
+int n;
+int Arr[n>=i];
 
-        int n;
+int main(){
+
         printf("Please enter integer value for (n):\n");
         scanf("%d", &n);
         printf("You entered %d\n", n);
 
-        if(n % 2 == 0){
-                int f = n/2;
-                printf("%d",f);
+        /*fun(&n);
+        printf("%d\n", n);*/
+        int i;
+        int c;
+        for (i=1; n>=i;){
+                Arr[n] = fun(&n);
+                printf("\n%d,", Arr[n]);
+        }
+        /*for(c=0; c<40; c++){ 
+		printf("%d,", n );
+        printf("\n%d,", n);
+        }*/
+}
+
+void fun(int *n){
+
+        if(*n % 2 == 0){
+                *n = *n/2;
+                /*printf("%d",*n);*/
         }
         else{
-                int f = (3*n)+1;
-                printf("%d", f);
+                *n = (*n*3)+1;
+                /*printf("%d", *n);*/
         }
-}
+
