b42fd8a3ce210ed84b718ecb16a8912b1a129ee5
Christopher-Devitt <chris.devitt@ucdconnect.ie>
2021-11-15 16:20:08 +0000

    object file creation

---
+++ Practicals/practical06/Step6/mm.c
@@ -0,0 +1,13 @@
+void matrixMult(int n, int p, int q, double A[n][p], double B[p][q], double C[n][q]){
+
+    int i, j, k;
+
+    // Performing Matrix Multiplication //
+    for(i = 0; i < n; i++){
+        for(j = 0; j < q; j++){
+            for(k = 0; k < p; k++){
+                C[i][j] += A[i][k]*B[k][j];
+            }
+        }
+    }
+}   
