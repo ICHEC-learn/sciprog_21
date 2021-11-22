0bf92ca05d66a6bac3b9db77ce7a8875adc7517e
Aoife573 <51859634+Aoife573@users.noreply.github.com>
2021-11-22 17:48:49 +0000

    update index of for loops

--- Practical_06/matrix.c
+++ Practical_06/matrix.c
@@ -1,37 +1,37 @@
-include<stdio.h>
-/*declare all needed variables*/
-int i,j,k;
-int n=5, p=3, q=4;
+#include<stdio.h>
 
 int main(){
-	/*declare arrays*/
+        /*declare all needed variables*/
+        int i,j,k;
+        int n=5, p=3, q=4;
+        /*declare arrays*/
         double A[n][p], B[p][q], C[n][q];
-	/*create A matrix*/
+        /*create A matrix*/
         for(i=0; i<n; i++){
                 for(j=0; j<p; j++){
                         A[i][j]=i+j;
                 }
         }
-	/*create B matrix*/
+        /*create B matrix*/
         for(i=0; i<p; i++){
                 for(j=0; j<q; j++){
                         B[i][j]=i-j;
                 }
         }
-	/*create empty C matrix*/
+        /*create empty C matrix*/
         for(i=0; i<n; i++){
                 for(j=0; j<q; j++){
                         C[i][j]=0.0;
                 }
         }
 
-	/*multiple A matrix with B matrix to get C matrix*/
+        /*multiple matrix A and matrix B to get matrix C*/
         for(i=0; i<n; i++)
                 for(j=0; j<q; j++)
                         for(k=0; k<p; k++)
-                                C[i][j] = C[i][j]+B[i][j]*A[i][j];
-	/*print all arrays*/
-        printf("Matrix A:\n");
+                                C[i][j] = C[i][j]+A[i][k]*B[k][j];
+        /*print out all the matrices*/
+        printf("\nMatrix A:\n\n");
         for(i=0; i<n; i++){
                 for(j=0; j<p; j++){
                         printf("%3.0f", A[i][j]);
@@ -39,22 +39,16 @@ int main(){
                 printf("\n");
         }
 
-        printf("Matrix B:\n");
-        for(i=0; i<n; i++){
-                for(j=0; j<p; j++){
+        printf("\nMatrix B:\n\n");
+        for(i=0; i<p; i++){
+                for(j=0; j<q; j++){
                         printf("%3.0f", B[i][j]);
         }
                 printf("\n");
-        }
-                                                                                                                                                           1,1           Top
-	printf("Matrix C:\n");
-        for(i=0; i<n; i++){
-                for(j=0; j<p; j++){
-                        printf("%3.0f", C[i][j]);
-        }
-                printf("\n");
-        }
+		
+	}
         return 0;
 
 }
-
+                 
+                                                                                                                                                           1,16          Top
